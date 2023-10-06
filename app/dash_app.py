eac5d696602376166473b56e9add123733ef89f2
Julian DeVille <53p3n7@gmail.com>
2023-10-06 12:12:04 -0700

    removed some unused imports

--- app/dash_app.py
+++ app/dash_app.py
@@ -1,9 +1,7 @@
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
-import numpy as np
 import plotly.graph_objs as go
-import pandas as pd
 
 from read_data import read_data
 
